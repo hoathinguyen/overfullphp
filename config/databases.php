a8cf6072c020b7859a4244645ca56106cf4bb655
Toi Tran Lam <Toi Tran Lam>
2017-01-01 11:33:21 +0700

    Fixed array

--- config/databases.php
+++ config/databases.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'use' => 'server',
+	'use' => 'overfull',
 
 	'connections' => [
 
@@ -11,36 +11,6 @@ return [
 			'host' => 'localhost',
 			'type' => 'mysql', //pgsql
 			'dbname' => 'overfull',
-			'encoding' => 'utf8'
-		],
-
-		'share' => [
-			'user' => 'root',
-			'password' => '',
-			'port' => '80',
-			'host' => 'localhost',
-			'type' => 'mysql', //pgsql
-			'dbname' => 'share',
-			'encoding' => 'utf8'
-		],
-            
-                'server' => [
-			'user' => 'inamlim9_overful',
-			'password' => 'Tltttml14112109',
-			'port' => '80',
-			'host' => '103.18.6.177',
-			'type' => 'mysql', //pgsql
-			'dbname' => 'inamlim9_overfull',
-			'encoding' => 'utf8'
-		],
-            
-                'server_share' => [
-			'user' => 'inamlim9_share',
-			'password' => 'Tltttml14112109',
-			'port' => '80',
-			'host' => '103.18.6.177',
-			'type' => 'mysql', //pgsql
-			'dbname' => 'inamlim9_share',
 			'encoding' => 'utf8'
 		]
 	]
